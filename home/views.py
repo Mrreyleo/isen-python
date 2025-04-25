172196fd6e65036ef903e7d1d987eda699c1a268
mrreyleo <98902675+Mrreyleo@users.noreply.github.com>
2025-04-25 11:39:42 +0200

    fix view2

--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, redirect
+from decimal import Decimal, InvalidOperation
+from django.shortcuts import redirect
 from django.views.generic import ListView
 from products.models import Product
-from decimal import Decimal, InvalidOperation
 
 # Redirection de la racine vers /home/
 def RedirectHomeView(request):
@@ -9,6 +9,7 @@ def RedirectHomeView(request):
     Redirige l'URL de '/' vers '/home/'
     '''
     return redirect('home')
+
 
 
 class HomeView(ListView):
@@ -25,12 +26,16 @@ class HomeView(ListView):
         try:
             if min_price:
                 queryset = queryset.filter(price__gte=Decimal(min_price))
+        except (InvalidOperation, ValueError):
+            pass  # Ignorer si min_price est invalide
+
+        try:
             if max_price:
                 queryset = queryset.filter(price__lte=Decimal(max_price))
         except (InvalidOperation, ValueError):
-            # En cas de filtre invalide, ignorer simplement les filtres
-            queryset = Product.objects.none()
+            pass  # Ignorer si max_price est invalide
 
         self.object_list = queryset
         context = self.get_context_data(object_list=self.object_list)
         return self.render_to_response(context)
+
